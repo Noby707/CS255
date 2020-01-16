46b47dd62b74e79d46ba3bbb318c75545c72799c
Ahmed Osman <Noby707@users.noreply.github.com>
2020-01-15 23:06:30 -0500

    Lab1
    First Lab assignments in CS255

---
+++ Hello.java
@@ -0,0 +1,22 @@
+/******************************************
+* Hello.java
+*******************************************
+* Ahmed Osman
+* 01/15/2020
+* CS 255 Section 901
+*******************************************/
+
+
+public class Hello {
+
+    public static void main(String[] args) {
+    	
+        System.out.println("Ahmed Osman");
+        System.out.println("Hello.java");
+        System.out.println("A java program to test that java works");
+        System.out.println("Java version 1.8.0_242");
+        System.out.println("CS 255 Section 901");
+        System.out.println();
+    }
+
+}
